4cafd13874fb1a3d8c137fbf204a795f56bd54af
rrahn <rene.rahn@fu-berlin.de>
2018-02-15 10:31:33 +0100

    [NOP] Change copyright year.

--- src/align_bench_options.hpp
+++ src/align_bench_options.hpp
@@ -1,7 +1,7 @@
 // ==========================================================================
 //                              Align_Bench
 // ==========================================================================
-// Copyright (c) 2006-2015, Knut Reinert, FU Berlin
+// Copyright (c) 2006-2018, Knut Reinert, FU Berlin
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
