f6accc9fa3703c197bc15436e122579b74109e26
rrahn <rene.rahn@fu-berlin.de>
2020-01-24 18:22:12 +0100

    [MISC] Test direct invocation of alignment function object.

--- src/align_bench3_parallel.cpp
+++ src/align_bench3_parallel.cpp
@@ -6,8 +6,10 @@
 #include <seqan3/alignment/scoring/nucleotide_scoring_scheme.hpp>
 #include <seqan3/alphabet/nucleotide/dna5.hpp>
 #include <seqan3/io/sequence_file/input.hpp>
+#include <seqan3/range/views/chunk.hpp>
 #include <seqan3/range/views/pairwise_combine.hpp>
 #include <seqan3/range/views/take.hpp>
+#include <seqan3/range/views/type_reduce.hpp>
 #include <seqan3/range/views/zip.hpp>
 
 int main(int const argc, char const ** argv)
@@ -27,16 +29,27 @@ int main(int const argc, char const ** argv)
                          seqan3::align_cfg::gap{gap_scheme} |
                          seqan3::align_cfg::result{seqan3::with_score, seqan3::using_score_type<int16_t>};
 
-    auto vectorised_configuration = configuration | seqan3::align_cfg::vectorise;
+    // auto vectorised_configuration = configuration | seqan3::align_cfg::vectorise;
 
     std::vector<int16_t> scores{};
 
     auto start_time = std::chrono::high_resolution_clock::now();
-    auto sequence_pairs = seqan3::views::pairwise_combine(sequences | seqan3::views::take(1000));
-    // auto sequence_pairs = seqan3::views::zip(sequences, sequences);
 
-    for (auto const & alignment_result : seqan3::align_pairwise(sequence_pairs, configuration))
-        scores.push_back(alignment_result.score());
+    auto sequence_pairs = seqan3::views::pairwise_combine(sequences);
+    // Configure the alignment algorithm.
+    auto && [algorithm, adapted_config] = seqan3::detail::alignment_configurator::configure<decltype(sequence_pairs)>(configuration);
+
+    size_t index{0};
+    for (auto && [seq1, seq2] : sequence_pairs)
+        scores.push_back(algorithm(index, seq1, seq2).score());
+
+    // for (auto && indexed_sequence_pair : seqan3::views::zip(sequence_pairs, std::views::iota(0)) | seqan3::views::chunk(1000))
+    //     for (auto const & res : algorithm(indexed_sequence_pair))
+    //         scores.push_back(res.score());
+
+        // (algorithm(indexed_sequence_pair)[0].score());
+    // for (auto const & alignment_result : seqan3::align_pairwise(sequence_pairs, configuration))
+    //     scores.push_back(alignment_result.score());
 
     auto end_time = std::chrono::high_resolution_clock::now();
     std::cout << std::chrono::duration_cast<std::chrono::seconds>(end_time - start_time).count() << "s\n";
