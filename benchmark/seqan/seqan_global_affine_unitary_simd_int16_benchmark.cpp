e7b51ad825488888a25af51f3631c78a9f41fe2c
rrahn <rene.rahn@fu-berlin.de>
2023-03-22 16:40:07 +0100

    Add seqan2 simd tests with fixed int16_t score type

---
+++ benchmark/seqan/seqan_global_affine_unitary_simd_int16_benchmark.cpp
@@ -0,0 +1,23 @@
+#include <benchmark/benchmark.h>
+
+#include <seqan/align_parallel.h>
+
+#include "affine_bench_fixture.hpp"
+
+BENCHMARK_TEMPLATE_F(affine_bench_fixture,
+                     seqan_global_affine_unitary_simd_int16,
+                     seqan::Dna5,
+                     int16_t)(benchmark::State& state) {
+
+    using exec_policy_t = seqan::ExecutionPolicy<seqan::Serial, seqan::Vectorial>;
+    using end_gaps_t = seqan::AlignConfig<false, false, false, false>;
+
+    seqan::String<score_type> res{};
+    for (auto _ : state) {
+        res = seqan::globalAlignmentScore(exec_policy_t{}, sequence1(), sequence2(), scoring_scheme(), end_gaps_t{});
+    }
+
+    benchmark::DoNotOptimize(res);
+}
+
+BENCHMARK_MAIN();
---
+++ benchmark/seqan/seqan_overlap_affine_unitary_simd_int16_benchmark.cpp
@@ -0,0 +1,24 @@
+#include <benchmark/benchmark.h>
+
+#include <seqan/align_parallel.h>
+
+#include "affine_bench_fixture.hpp"
+
+BENCHMARK_TEMPLATE_F(affine_bench_fixture,
+                     seqan_overlap_affine_unitary_simd_int16,
+                     seqan::Dna5,
+                     int16_t)(benchmark::State& state) {
+
+    using exec_policy_t = seqan::ExecutionPolicy<seqan::Serial, seqan::Vectorial>;
+    using end_gaps_t = seqan::AlignConfig<true, true, true, true>;
+
+    seqan::String<score_type> res{};
+    for (auto _ : state) {
+        res = seqan::globalAlignmentScore(exec_policy_t{}, sequence1(), sequence2(), scoring_scheme(), end_gaps_t{});
+    }
+
+    benchmark::DoNotOptimize(res);
+}
+
+// Run the benchmark
+BENCHMARK_MAIN();
