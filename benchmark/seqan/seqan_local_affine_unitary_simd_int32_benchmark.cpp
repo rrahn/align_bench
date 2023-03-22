1899b2bb9caca31ca07c1b0280fd2a09037f8abd
rrahn <rene.rahn@fu-berlin.de>
2023-03-22 16:28:05 +0100

    Add seqan2 simd benchmarks

---
+++ benchmark/seqan/seqan_local_affine_unitary_simd_int32_benchmark.cpp
@@ -0,0 +1,23 @@
+#include <benchmark/benchmark.h>
+
+#include <seqan/align_parallel.h>
+
+#include "affine_bench_fixture.hpp"
+
+BENCHMARK_TEMPLATE_F(affine_bench_fixture,
+                     seqan_local_affine_unitary_simd_int32,
+                     seqan::Dna5,
+                     int32_t)(benchmark::State& state) {
+
+    using exec_policy_t = seqan::ExecutionPolicy<seqan::Serial, seqan::Vectorial>;
+
+    seqan::String<score_type> res{};
+    for (auto _ : state) {
+        res = seqan::localAlignmentScore(exec_policy_t{}, sequence1(), sequence2(), scoring_scheme());
+    }
+
+    benchmark::DoNotOptimize(res);
+}
+
+// Run the benchmark
+BENCHMARK_MAIN();
